aee503fab9eec92ba755afcda4f723d85f71d899
ZainabRaheem <zainabraheem1204@gmail.com>
2022-12-12 08:38:05 +0100

    A lot of changes

---
+++ LMS Workshop/Week11/index.js
@@ -0,0 +1,16 @@
+// alert("Hello");
+
+console.log("Hello!");
+// prompt("What is your name?");
+
+var userName;
+userName = "Ada";
+
+var emailAddress = "adanaza89@gmail.com";
+let subject = "JavaScript";
+emailAddress = "ada@gmail.com";
+const greeting = "Hello!";
+
+console.log(emailAddress, greeting);
+
+// alert(emailAddress);
---
+++ Workshop/Week11/index.js
@@ -0,0 +1,40 @@
+let myArray = [];
+console.log(typeof myArray);
+
+let dog = "bullet";
+console.log(typeof dog);
+
+let myObject = {
+  Program: "Ada Internship",
+  Cohort: 1,
+  Track: "Front-end",
+};
+
+console.log(myObject.Program);
+
+console.log(typeof true);
+
+let rate;
+console.log(typeof rate);
+
+// let books = [
+//   2,
+//   3,
+//   "name",
+//   { Program: "Ada Internship", Cohort: 1, Track: "Front-end" },
+// ];
+// console.log(books[3]);
+
+// Arithmetic Operations in JavaScript
+2 + 2;
+2 - 2;
+2 * 2;
+2 / 2;
+console.log(3 % 2);
+
+let a = "10";
+
+console.log(a !== 10);
+
+if (a == 8 && dog === "bullet") {
+}
